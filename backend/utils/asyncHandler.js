1574fe7ae5d23645e48ae59f85ffc3c4ce79c5f4
Nishanth <msnishanth45@gmail.com>
2026-03-24 15:50:04 +0530

    feat: upgrade to options paper trading platform (clean build)

--- backend/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Wraps an async Express route handler to automatically
- * catch errors and forward them to the error middleware.
- */
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
